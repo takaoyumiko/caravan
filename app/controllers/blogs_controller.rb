34413b31c76c7764a679f3f3a41c5576da4d4e5a
takaoyumiko <takaoyumiko09@gmail.com>
2018-03-14 10:32:25 +0000

    week3

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,27 @@
 class BlogsController < ApplicationController
 	def index
-
+		@blogs = Blog.all
 	end
+
+	def new
+		@blog = Blog.new
+	end
+
+	def show
+		@blog = Blog.find(params[:id])
+	end
+
+	def create
+		blog = Blog.new(blog_params)
+		blog.save
+		redirect_to "/blogs"
+	end
+
+
+
+	private
+	def blog_params
+	    params.require(:blog).permit(:title, :body, :category)
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/blogs' => 'blogs#index'
+  get '/blogs/new' => 'blogs#new'
+  post '/blogs' => 'blogs#create'
+  get '/blogs' => 'blogs#index'
+  get '/blogs/:id' => 'blogs#show'
 end
